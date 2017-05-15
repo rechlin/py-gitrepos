8f9ca592c03d8cc6011b5f0e486122570d9b1905
Rob Echlin <rob@echlin.ca>
2017-05-15 13:43:45 -0400

    rename lib file

--- repo-report.py
+++ repo-report.py
@@ -5,13 +5,13 @@ import fileinput
 import sys
 import glob
 import argparse
-from git_repo_lib import repoListFromFile, processRepoList
+from lib_git_repo import repoListFromFile, processRepoList
 
-debug = False
+debug = True
 
 
 def repoSearch(topDir):
-    """ Return a list of paths to git repos in topDir and sub folders """
+    """ Return a list of paths to git repos in topDir """
     results = []
     topDirGlob = topDir + '/**/.git'
     if debug:
@@ -26,67 +26,19 @@ def repoSearch(topDir):
 def args(listFile):
     """ Check input, return path or other info """
     parser = argparse.ArgumentParser()
-    parser.addargument('path', 
-            nargs='+',
-            help="Path(s) to one or more folders that contain one or more git repos")
+    parser.add_argument('path', 
+            nargs='*',
+            help="Path(s) to one or more folders that contain one or more git repos",
+            default=argparse.SUPPRESS)
     
     args = parser.parse_args()
-    
-
-def deadcode2():
-    """ More code to harvest from in args() """
-    if debug:
-        print("num inputs: ", numArgs)
-        print("script: ", sys.argv[0])
-
-    if numArgs == 1:
-        if os.path.exists(listFile):
-            return('listFile')
-        else:
-            return('noListFile')
-    elif numArgs > 1:
-        for arg in sys.argv[1:]:
-            if debug:
-                print("This arg: ", arg)
-            
-            if "=" in sys.argv[1]:
-                param, topDir = sys.argv[1].split("=")
-                
-                if debug:
-                    print("    param: ", param)
-                    print("    value: ", topDir)
-                
-                if param == "topDir":
-                    if os.path.exists(topDir):
-                        print('Processing files in top folder: ', topDir)
-                        repoList = repoSearch(topDir)
-                        
-                        if debug:
-                            print('repolist from search of topDir: ', topDir)
-                            print('    ',repoList)
-                            
-                        processRepoList(repoList)
-                    else:
-                        print('Error: top folder not found: ', topDir)
-                        return()
-                            
-                else:
-                    print("param is not topDir")
-                    processRepoList(repoListFile)
-                
-    else:
-        if debug:
-            print('no params on command line')
-            print('repoListFile: ', repoListFile)
-
-        repoList = repoListFromFile(repoListFile)
-        processRepoList(repoList)
+    return(args)
     
 
 
 def helpMsg(listFile):
     """ Print help message """
-    print('No param: process the files in the default help file: ',listfile)
+    print('No param: process the files in the default help file: ',listFile)
     print('One param: name of a folder that has a git repo or folders with repos')
     print('First param is help, --help, -h: print this message')
 
@@ -97,6 +49,9 @@ def main():
     repoListFile = ScriptFolder + "/" + ListFileName
 
     toDo = args(repoListFile)
+
+    if debug:
+        print('args is: ', toDo)
     
     if toDo == 'noListFile':
         print('The list file was not found')
@@ -107,62 +62,14 @@ def main():
         processRepoList(repoList)
     
     else:
-        helpMsg(repoListFile)
-        
-    return()
-    
-    
-def deadcode():
-    ''' copy from here for cliInput and toDo '''
-    numArgs = len(sys.argv)
-
-    if debug:
-        print("num inputs: ", numArgs)
-        print("script: ", sys.argv[0])
-
-    if numArgs > 1:
-        if sys.argv[1] in ['-h', '--help']:
-            print('testing')
-        for arg in sys.argv[1:]:
-            if debug:
-                print("This arg: ", arg)
-            
-            if "=" in sys.argv[1]:
-                param, topDir = sys.argv[1].split("=")
-                
-                if debug:
-                    print("    param: ", param)
-                    print("    value: ", topDir)
-                
-                if param == "topDir":
-                    if os.path.exists(topDir):
-                        print('Processing files in top folder: ', topDir)
-                        repoList = repoSearch(topDir)
-                        
-                        if debug:
-                            print('repolist from search of topDir: ', topDir)
-                            print('    ',repoList)
-                            
-                        processRepoList(repoList)
-                    else:
-                        print('Error: top folder not found: ', topDir)
-                        return()
-                            
-                else:
-                    print("param is not topDir")
-                    processRepoList(repoListFile)
-                
-    else:
-        if debug:
-            print('no params on command line')
-            print('repoListFile: ', repoListFile)
-
         repoList = repoListFromFile(repoListFile)
         processRepoList(repoList)
-    
-    print("repos end")
         
+    return()
+
+
 
 #----------------------------------
 if __name__ == "__main__":
     main()
+
