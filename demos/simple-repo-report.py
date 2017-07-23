da847e2507cf2871a8f02b8ad8f8d088fdd628cf
Rob Echlin <rob@echlin.ca>
2017-07-22 21:16:00 -0400

    move simple report app to demos folder

---
+++ demos/simple-repo-report.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import fileinput
+
+repoListFile = "projects-git.repos.txt"
+repoList = []
+
+with fileinput.input(repoListFile) as f:
+    for line in f:
+        repoList.append(line.strip())
+
+for path in repoList:
+    print('  ',path)
