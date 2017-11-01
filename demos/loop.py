49a2dfd8c754f0a95bbca68828796ab3d09cceeb
Rob Echlin <rob@echlin.ca>
2017-11-01 15:18:41 -0400

    loop.py, simplify

--- demos/loop.py
+++ demos/loop.py
@@ -8,15 +8,9 @@ import fileinput
 from colors import color
 import time
 
-string = []
-
-# get the text
-with fileinput.input("loop.txt") as f:
-    for line in f:
-        string.append(line.strip())
 
 # output some of it
-myString = string[0]
+myString = "Fiber Natcchos? Series-ly?"
 print( '')
 
 # extra vars
@@ -28,16 +22,11 @@ a, b = 0, 1
 while b <= len(myString):
     a, b = b, a + b
     # print('{} {}'.format(a, b))
-    
+
     # increasing delay, by Fibonacci
     time.sleep(0.05 * b)
-    
+
     # color choice by count, length by Fibonacci
     print(color(myString[0:b], fg=colorNum))
     print()
     colorNum += 2
-    
-       
-    
-    
-    
